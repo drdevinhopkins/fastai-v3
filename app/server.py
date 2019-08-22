f8d420fb49baa439de7eceedbb6615c04f786540
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2019-08-22 13:31:59 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,17 +9,17 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-#export_file_url = 'https://www.dropbox.com/s/g03mq3xblwryb4f/export.pkl?dl=1'
-#export_file_name = 'export.pkl'
-
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/g03mq3xblwryb4f/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
-path = Path(__file__).parent
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_name = 'export.pkl'
 
-#classes = ['atelectasis','cardiomegaly','consolidation','edema','enlargedCardiomediastinum','fracture','lungLesion','lungOpacity','noFinding','pleuralEffusion','pleuralOther','pneumonia','pneumothorax','supportDevices']
+#classes = ['black', 'grizzly', 'teddys']
 #path = Path(__file__).parent
+
+classes = ['atelectasis','cardiomegaly','consolidation','edema','enlargedCardiomediastinum','fracture','lungLesion','lungOpacity','noFinding','pleuralEffusion','pleuralOther','pneumonia','pneumothorax','supportDevices']
+path = Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
