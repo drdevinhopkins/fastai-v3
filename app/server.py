a51b2641369f92527cca0dc6311fc5e04745e035
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2019-08-22 03:27:18 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,23 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/g03mq3xblwryb4f/testmodel.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['atelectasis',
+ 'cardiomegaly',
+ 'consolidation',
+ 'edema',
+ 'enlargedCardiomediastinum',
+ 'fracture',
+ 'lungLesion',
+ 'lungOpacity',
+ 'noFinding',
+ 'pleuralEffusion',
+ 'pleuralOther',
+ 'pneumonia',
+ 'pneumothorax',
+ 'supportDevices']
 path = Path(__file__).parent
 
 app = Starlette()
